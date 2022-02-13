7113e1661318b7af97359e8c13605188f186940b
Fábio A. M. Pereira <famp@operamail.com>
2022-02-13 11:59:48 -0300

    - Added an import for the semantic ui css on the index file (we need it for the library to work) - Modified the TextStatistics component to be stylized with the semantic ui library. - Also moved the css to a separate folder where all the css files are going to be stored from now on.

--- src/components/TextStatistics.tsx
+++ src/components/TextStatistics.tsx
@@ -1,16 +1,25 @@
-import "./TextStatistics.css";
+import { Segment, Statistic } from "semantic-ui-react";
+import "../css/TextStatistics.css";
 
 const TextStatistics = (props: TextStatisticsProps) => {
   return (
     <div className="statisticsContainer">
-      <p>
-        <span className="description">Vowels: </span>
-        {props.vowels}
-        <span className="description"> / Consonants: </span>
-        {props.consonants}
-        <span className="description"> / Words: </span>
-        {props.words}
-      </p>
+      <Segment padded>
+        <Statistic.Group size="tiny" widths={3}>
+          <Statistic>
+            <Statistic.Value>{props.vowels}</Statistic.Value>
+            <Statistic.Label>Vowels</Statistic.Label>
+          </Statistic>
+          <Statistic>
+            <Statistic.Value>{props.consonants}</Statistic.Value>
+            <Statistic.Label>Consonants</Statistic.Label>
+          </Statistic>
+          <Statistic>
+            <Statistic.Value>{props.words}</Statistic.Value>
+            <Statistic.Label>Words</Statistic.Label>
+          </Statistic>
+        </Statistic.Group>
+      </Segment>
     </div>
   );
 };
