811f42488843291b1fe78c66fccf15c239a508da
Fábio A. M. Pereira <famp@operamail.com>
2022-02-13 23:29:29 -0300

    - Finally this is the modified composer, where you can edit the text. It also deals with hotkeys pressed and the vowels, consonants and words calculation.

--- src/components/Composer.tsx
+++ src/components/Composer.tsx
@@ -1,46 +1,110 @@
-import React, { useState, useMemo, useCallback } from "react";
-import { createEditor, Node } from "slate";
+import { useState, useMemo, useCallback, KeyboardEvent } from "react";
+import { createEditor, Node, Descendant } from "slate";
 // See docs and examples at https://docs.slatejs.org
-import {
-  Slate,
-  Editable,
-  ReactEditor,
-  withReact,
-  RenderElementProps,
-} from "slate-react";
+import { Slate, Editable, withReact, useSlate } from "slate-react";
+import { alignLeftAction, alignCenterAction, alignRightAction, justifyAction } from "../actions/Actions";
+import { connect } from "react-redux";
+import { AnyAction } from "redux";
+import isHotkey from 'is-hotkey'
 import TextStatistics from "./TextStatistics";
-import "./Composer.css";
+import RichTextButtons from "./RichTextButtons";
+import { countMatches, HOTKEYS, Element, Leaf } from "../utils/Utils";
+import { withHistory } from 'slate-history'
+import "../css/Composer.css";
 
-const Element = ({ attributes, children }: RenderElementProps) => {
-  return <p {...attributes}>{children}</p>;
-};
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  }
+];
 
-const Composer = () => {
-  const [value, setValue] = useState<Node[]>(initialValue);
-  const renderElement = useCallback((props) => <Element {...props} />, []);
-  const onChange = useCallback((value) => setValue(value), []);
-  const editor = useMemo(() => withReact(createEditor() as ReactEditor), []);
+const Composer = (props: AnyAction) => {
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const [vowelsCount, setVowelsCount] = useState<number>(countMatches(Node.string(initialValue[0]), "vowels"));
+  const [consonantsCount, setConsonantsCount] = useState<number>(countMatches(Node.string(initialValue[0]), "consonants"));
+  const [wordsCount, setWordsCount] = useState<number>(countMatches(Node.string(initialValue[0]), "words"));
+
+  const updateValues = (value: Descendant[]) => {
+    let composerText: string = Node.string(value[0]);
+    setValue(value);
+    setVowelsCount(countMatches(composerText, "vowels"));
+    setConsonantsCount(countMatches(composerText, "consonants"));
+    setWordsCount(countMatches(composerText, "words"));
+  }
+
+  const renderElement = useCallback(props => <Element {...props} />, []);
+  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+  const onChange = useCallback((value) => updateValues(value), []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+
   return (
-    <Slate editor={editor} value={value} onChange={onChange}>
-      <div className="composer">
-        <Editable
-          renderElement={renderElement}
-          className="editable"
-          placeholder="Enter some plain text..."
+    <>
+      <Slate editor={editor} value={value} onChange={onChange}>
+        <RichTextButtons editor={useSlate} />
+        <div className="composer">
+          <Editable
+            className="editable"
+            style={{ textAlign: props.textAlign }}
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            placeholder="Enter some text..."
+            autoFocus
+            spellCheck={false}
+            onKeyDown={event => handleKeyDownPress(event, props)}
+          />
+        </div>
+        <TextStatistics
+          consonants={consonantsCount}
+          vowels={vowelsCount}
+          words={wordsCount}
         />
-        <TextStatistics />
-      </div>
-    </Slate>
+      </Slate>
+    </>
   );
 };
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [
-      { text: "This is editable plain text, just like a <textarea>!" },
-    ],
-  },
-];
+const handleKeyDownPress = (event: KeyboardEvent<HTMLDivElement>, props: AnyAction) => {
+  for (const hotkey in HOTKEYS) {
+    if (isHotkey(hotkey, event as any)) {
+      event.preventDefault();
+      switch (HOTKEYS[hotkey]) {
+        case "left":
+          props.onAlignLeft();
+          break;
+        case "center":
+          props.onAlignCenter();
+          break;
+        case "right":
+          props.onAlignRight();
+          break;
+        case "justify":
+          props.onJustify();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
 
-export default Composer;
+const mapStateToProps = (state: { textAlign: string; }) => ({
+  textAlign: state.textAlign
+});
+
+const mapActionsToProps = {
+  onAlignLeft: alignLeftAction,
+  onAlignCenter: alignCenterAction,
+  onAlignRight: alignRightAction,
+  onJustify: justifyAction,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Composer);
