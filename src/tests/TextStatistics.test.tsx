ed4abeb1b85fcb2036dbab8e85d640765b796fe0
Fábio A. M. Pereira <famp@operamail.com>
2022-02-15 22:46:24 -0300

    - Added a few more tests to the TextStatistics component.

--- src/tests/TextStatistics.test.tsx
+++ src/tests/TextStatistics.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import TextStatistics from "../components/TextStatistics";
 
-it("Expect vowel counters to be in the document", () => {
+it("Expect text statistics to be in the document", () => {
   render(
     <TextStatistics
       vowels={1}
@@ -20,4 +20,46 @@ it("Expect vowel counters to be in the document", () => {
   expect(numberOfConsonantsElement).toBeInTheDocument();
   expect(numberOfWordsElement).toBeInTheDocument();
   expect(numberOfDigitsElement).toBeInTheDocument();
-})
+});
+
+it("Expect text statistics to have value class", () => {
+  render(
+    <TextStatistics
+      vowels={1}
+      consonants={2}
+      words={9}
+      digits={18}
+    />
+  );
+
+  const numberOfVowelsElement = screen.getByText("1");
+  const numberOfConsonantsElement = screen.getByText("2");
+  const numberOfWordsElement = screen.getByText("9");
+  const numberOfDigitsElement = screen.getByText("18");
+
+  expect(numberOfVowelsElement).toHaveClass("value");
+  expect(numberOfConsonantsElement).toHaveClass("value");
+  expect(numberOfWordsElement).toHaveClass("value");
+  expect(numberOfDigitsElement).toHaveClass("value");
+});
+
+it("Expect text statistics text content to match passed props", () => {
+  render(
+    <TextStatistics
+      vowels={1}
+      consonants={2}
+      words={9}
+      digits={18}
+    />
+  );
+
+  const numberOfVowelsElement = screen.getByText("1");
+  const numberOfConsonantsElement = screen.getByText("2");
+  const numberOfWordsElement = screen.getByText("9");
+  const numberOfDigitsElement = screen.getByText("18");
+
+  expect(numberOfVowelsElement).toHaveTextContent("1");
+  expect(numberOfConsonantsElement).toHaveTextContent("2");
+  expect(numberOfWordsElement).toHaveTextContent("9");
+  expect(numberOfDigitsElement).toHaveTextContent("18");
+});
