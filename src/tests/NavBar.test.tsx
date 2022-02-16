3f2f2512c92253f4125e3521245ecfdc7f12b2b4
Fábio A. M. Pereira <famp@operamail.com>
2022-02-15 22:50:48 -0300

    - Added another test and modified the already existing ones.

--- src/tests/NavBar.test.tsx
+++ src/tests/NavBar.test.tsx
@@ -4,21 +4,26 @@ import NavBar from "../components/NavBar";
 it("Expect the header and image to be in the document", () => {
   render(<NavBar />);
 
-  const headingElement = screen.getByRole("heading", { name: "Composer" });
+  const headingElement = screen.getByRole("heading");
   const imageElement = screen.getByRole("img");
 
   expect(headingElement).toBeInTheDocument();
-  expect(headingElement).toHaveClass("ui header navbarTitle");
   expect(imageElement).toBeInTheDocument();
-  expect(imageElement).toHaveClass("ui small image");
-})
+});
 
 it("Expect the header and image to have the appropriate classes", () => {
   render(<NavBar />);
 
-  const headingElement = screen.getByRole("heading", { name: "Composer" });
+  const headingElement = screen.getByRole("heading");
   const imageElement = screen.getByRole("img");
 
   expect(headingElement).toHaveClass("ui header navbarTitle");
   expect(imageElement).toHaveClass("ui small image");
-})
+});
+
+it("Expect the header to have the exact text 'Composer'", () => {
+  render(<NavBar />);
+
+  const headingElement = screen.getByRole("heading");
+  expect(headingElement).toHaveTextContent("Composer");
+});
