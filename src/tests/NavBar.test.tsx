55b248ed6975f3d53528a47fa7367708fd785357
Fábio A. M. Pereira <famp@operamail.com>
2022-02-15 23:12:00 -0300

    - Wrapped the tests into specific describe blocks.

--- src/tests/NavBar.test.tsx
+++ src/tests/NavBar.test.tsx
@@ -1,29 +1,41 @@
 import { render, screen } from "@testing-library/react";
 import NavBar from "../components/NavBar";
 
-it("Expect the header and image to be in the document", () => {
-  render(<NavBar />);
+describe("Header tests", () => {
+  it("Expect the header to be in the document", () => {
+    render(<NavBar />);
+  
+    const headingElement = screen.getByRole("heading");  
+    expect(headingElement).toBeInTheDocument();
+  });
 
-  const headingElement = screen.getByRole("heading");
-  const imageElement = screen.getByRole("img");
+  it("Expect the header to have the appropriate classe", () => {
+    render(<NavBar />);
+  
+    const headingElement = screen.getByRole("heading");  
+    expect(headingElement).toHaveClass("ui header navbarTitle");
+  });
 
-  expect(headingElement).toBeInTheDocument();
-  expect(imageElement).toBeInTheDocument();
+  it("Expect the header to have the exact text 'Composer'", () => {
+    render(<NavBar />);
+  
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toHaveTextContent("Composer");
+  });
 });
 
-it("Expect the header and image to have the appropriate classes", () => {
-  render(<NavBar />);
-
-  const headingElement = screen.getByRole("heading");
-  const imageElement = screen.getByRole("img");
-
-  expect(headingElement).toHaveClass("ui header navbarTitle");
-  expect(imageElement).toHaveClass("ui small image");
-});
-
-it("Expect the header to have the exact text 'Composer'", () => {
-  render(<NavBar />);
-
-  const headingElement = screen.getByRole("heading");
-  expect(headingElement).toHaveTextContent("Composer");
+describe("Image tests", () => {
+  it("Expect the image to be in the document", () => {
+    render(<NavBar />);
+  
+    const imageElement = screen.getByRole("img");
+    expect(imageElement).toBeInTheDocument();
+  });
+  
+  it("Expect the image to have the appropriate classes", () => {
+    render(<NavBar />);
+  
+    const imageElement = screen.getByRole("img");
+    expect(imageElement).toHaveClass("ui small image");
+  });
 });
--- src/tests/TextStatistics.test.tsx
+++ src/tests/TextStatistics.test.tsx
@@ -1,65 +1,67 @@
 import { render, screen } from "@testing-library/react";
 import TextStatistics from "../components/TextStatistics";
 
-it("Expect text statistics to be in the document", () => {
-  render(
-    <TextStatistics
-      vowels={1}
-      consonants={2}
-      words={9}
-      digits={18}
-    />
-  );
-
-  const numberOfVowelsElement = screen.getByText("1");
-  const numberOfConsonantsElement = screen.getByText("2");
-  const numberOfWordsElement = screen.getByText("9");
-  const numberOfDigitsElement = screen.getByText("18");
-
-  expect(numberOfVowelsElement).toBeInTheDocument();
-  expect(numberOfConsonantsElement).toBeInTheDocument();
-  expect(numberOfWordsElement).toBeInTheDocument();
-  expect(numberOfDigitsElement).toBeInTheDocument();
-});
-
-it("Expect text statistics to have value class", () => {
-  render(
-    <TextStatistics
-      vowels={1}
-      consonants={2}
-      words={9}
-      digits={18}
-    />
-  );
-
-  const numberOfVowelsElement = screen.getByText("1");
-  const numberOfConsonantsElement = screen.getByText("2");
-  const numberOfWordsElement = screen.getByText("9");
-  const numberOfDigitsElement = screen.getByText("18");
-
-  expect(numberOfVowelsElement).toHaveClass("value");
-  expect(numberOfConsonantsElement).toHaveClass("value");
-  expect(numberOfWordsElement).toHaveClass("value");
-  expect(numberOfDigitsElement).toHaveClass("value");
-});
-
-it("Expect text statistics text content to match passed props", () => {
-  render(
-    <TextStatistics
-      vowels={1}
-      consonants={2}
-      words={9}
-      digits={18}
-    />
-  );
-
-  const numberOfVowelsElement = screen.getByText("1");
-  const numberOfConsonantsElement = screen.getByText("2");
-  const numberOfWordsElement = screen.getByText("9");
-  const numberOfDigitsElement = screen.getByText("18");
-
-  expect(numberOfVowelsElement).toHaveTextContent("1");
-  expect(numberOfConsonantsElement).toHaveTextContent("2");
-  expect(numberOfWordsElement).toHaveTextContent("9");
-  expect(numberOfDigitsElement).toHaveTextContent("18");
+describe("Text statistics tests", () => {
+  it("Expect text statistics to be in the document", () => {
+    render(
+      <TextStatistics
+        vowels={1}
+        consonants={2}
+        words={9}
+        digits={18}
+      />
+    );
+  
+    const numberOfVowelsElement = screen.getByText("1");
+    const numberOfConsonantsElement = screen.getByText("2");
+    const numberOfWordsElement = screen.getByText("9");
+    const numberOfDigitsElement = screen.getByText("18");
+  
+    expect(numberOfVowelsElement).toBeInTheDocument();
+    expect(numberOfConsonantsElement).toBeInTheDocument();
+    expect(numberOfWordsElement).toBeInTheDocument();
+    expect(numberOfDigitsElement).toBeInTheDocument();
+  });
+  
+  it("Expect text statistics to have value class", () => {
+    render(
+      <TextStatistics
+        vowels={1}
+        consonants={2}
+        words={9}
+        digits={18}
+      />
+    );
+  
+    const numberOfVowelsElement = screen.getByText("1");
+    const numberOfConsonantsElement = screen.getByText("2");
+    const numberOfWordsElement = screen.getByText("9");
+    const numberOfDigitsElement = screen.getByText("18");
+  
+    expect(numberOfVowelsElement).toHaveClass("value");
+    expect(numberOfConsonantsElement).toHaveClass("value");
+    expect(numberOfWordsElement).toHaveClass("value");
+    expect(numberOfDigitsElement).toHaveClass("value");
+  });
+  
+  it("Expect text statistics text content to match passed props", () => {
+    render(
+      <TextStatistics
+        vowels={1}
+        consonants={2}
+        words={9}
+        digits={18}
+      />
+    );
+  
+    const numberOfVowelsElement = screen.getByText("1");
+    const numberOfConsonantsElement = screen.getByText("2");
+    const numberOfWordsElement = screen.getByText("9");
+    const numberOfDigitsElement = screen.getByText("18");
+  
+    expect(numberOfVowelsElement).toHaveTextContent("1");
+    expect(numberOfConsonantsElement).toHaveTextContent("2");
+    expect(numberOfWordsElement).toHaveTextContent("9");
+    expect(numberOfDigitsElement).toHaveTextContent("18");
+  });
 });
